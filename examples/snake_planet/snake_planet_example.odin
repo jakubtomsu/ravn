jakubtomsu/ravn

commit 4e84b0d5dc4fd1b81508f094ebacc5da62cdd0d4

remove math type aliases from the entire codebase

examples/snake_planet/snake_planet_example.odin
@@ -8,14 +8,14 @@ import "../../platform"
 import "../../base/ufmt"
 import "../../audio"
 
-SNAKE_RED :: rv.Vec4{1, 0.3, 0, 1}
+SNAKE_RED :: [4]f32{1, 0.3, 0, 1}
 SNAKE_ORANGE :: rv.ORANGE
 
 state: ^State
 
 State :: struct {
-    cam_pos:        rv.Vec3,
-    cam_rot:        rv.Quat,
+    cam_pos:        [3]f32,
+    cam_rot:        rv.quaternion128,
     cam_fov:        f32,
 
     obsts:          [64]Obstacle,
@@ -42,26 +42,26 @@ Screen_ID :: enum u8 {
 }
 
 Obstacle :: struct {
-    pos:    rv.Vec3,
+    pos:    [3]f32,
     rad:    f32,
 }
 
 Berry :: struct {
-    pos:    rv.Vec3,
+    pos:    [3]f32,
 }
 
 MAX_SEGMENTS :: 512
 
 Snake :: struct {
-    dir:            rv.Vec2,
-    pos:            rv.Vec3,
+    dir:            [2]f32,
+    pos:            [3]f32,
     segments:       [MAX_SEGMENTS]Segment,
     num_segments:   i32,
     segment_timer:  f32,
 }
 
 Segment :: struct {
-    pos:    rv.Vec3,
+    pos:    [3]f32,
 }
 
 @export _module_desc := rv.Module_Desc {
@@ -138,8 +138,8 @@ spawn_berry :: proc() {
     rv.create_sound(state.berry_sound, pitch = rand.float32_range(0.9, 1.2))
 }
 
-rand_dir :: proc() -> rv.Vec3 {
-    return linalg.normalize0(rv.Vec3{
+rand_dir :: proc() -> [3]f32 {
+    return linalg.normalize0([3]f32{
         rand.float32() * 2.0 - 1.0,
         rand.float32() * 2.0 - 1.0,
         rand.float32() * 2.0 - 1.0,
@@ -150,7 +150,7 @@ add_snake_segment :: proc() {
     snake := &state.snake
     assert(snake.num_segments > 0)
     pos := snake.num_segments == 0 ? snake.pos : snake.segments[snake.num_segments - 1].pos
-    offs: rv.Vec3
+    offs: [3]f32
     if snake.num_segments > 1 {
         offs = pos - snake.segments[snake.num_segments - 2].pos
     } else {
@@ -186,7 +186,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
         mat := linalg.matrix3_from_quaternion_f32(state.cam_rot)
 
         // TODO: Gamepad
-        move_inp: rv.Vec2
+        move_inp: [2]f32
 
         if rv.get_key_down(.D) do move_inp.x += 1
         if rv.get_key_down(.A) do move_inp.x -= 1
@@ -198,7 +198,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
         if rv.get_key_down(.Up) do move_inp.y += 1
         if rv.get_key_down(.Down) do move_inp.y -= 1
 
-        // move_dir := move_inp.x * rv.Vec3{1, 0, 0} + mat[1] * rv.Vec3{0, 1, 0}
+        // move_dir := move_inp.x * [3]f32{1, 0, 0} + mat[1] * [3]f32{0, 1, 0}
 
         if linalg.length2(move_inp) > 0.1 {
             move_inp = linalg.normalize0(move_inp)
@@ -285,7 +285,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
 
         sph := rv.get_builtin_mesh(.Icosphere_1)
 
-        rv.draw_mesh(sph, 0, col = rv.Vec4{0.0, 0.6, 0.2, 1})
+        rv.draw_mesh(sph, 0, col = [4]f32{0.0, 0.6, 0.2, 1})
 
         rv.set_draw_texture(rv.get_builtin_texture(.White))
 
@@ -297,8 +297,8 @@ _update :: proc(hot_state: rawptr) -> rawptr {
         }
 
         for obst in state.obsts[:state.num_obsts] {
-            rv.draw_mesh(sph, obst.pos, scale = obst.rad, col = rv.Vec4{0.0, 0.6, 0.2, 1})
-            rv.draw_mesh(sph, obst.pos * 1.1, scale = obst.rad * 1.2, col = rv.Vec4{0.2, 0.7, 0.3, 1})
+            rv.draw_mesh(sph, obst.pos, scale = obst.rad, col = [4]f32{0.0, 0.6, 0.2, 1})
+            rv.draw_mesh(sph, obst.pos * 1.1, scale = obst.rad * 1.2, col = [4]f32{0.2, 0.7, 0.3, 1})
         }
 
         rv.draw_mesh(sph, state.berry.pos * 1.1, scale = 0.2 + 0.05 * rv.nsin(rv.get_time() * 2), col = rv.RED)
@@ -363,7 +363,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     rv.draw_perf_scopes()
 
     rv.submit_layers()
-    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, rv.Vec3{0.05, 0.1, 0.2}, true)
+    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, [3]f32{0.05, 0.1, 0.2}, true)
     rv.render_layer(1, rv.DEFAULT_RENDER_TEXTURE, nil, false)
 
     // rv.end_frame(false) // disable VSYNC
@@ -371,7 +371,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     return state
 }
 
-repel_from_obstacles :: proc(pos: rv.Vec3, rad: f32) -> rv.Vec3 {
+repel_from_obstacles :: proc(pos: [3]f32, rad: f32) -> [3]f32 {
     pos := pos
     for obst in state.obsts[:state.num_obsts] {
         dist := linalg.length(pos - obst.pos)

examples/bit_pool_visualization/bit_pool_visualization_example.odin
@@ -22,8 +22,8 @@ State :: struct {
 }
 
 Particle :: struct {
-    pos:    rv.Vec2,
-    vel:    rv.Vec2,
+    pos:    [2]f32,
+    vel:    [2]f32,
     timer:  f32,
     dur:    f32,
 }
@@ -96,7 +96,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
                     rand.float32() * 2.0 - 1.0,
                     rand.float32() * 2.0 - 1.0,
                 },
-                vel = vel * rand.float32() * linalg.normalize0(rv.Vec2{
+                vel = vel * rand.float32() * linalg.normalize0([2]f32{
                     rand.float32() * 2.0 - 1.0,
                     rand.float32() * 2.0 - 1.0,
                 }),
@@ -186,7 +186,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
             assert(~block == 0)
         }
 
-        base_pos := rv.Vec3{
+        base_pos := [3]f32{
             64 + f32(i % 8) * (32 + 4),
             64 + f32(i / 8) * (32 + 4),
             0.1,
@@ -202,7 +202,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
         )
 
         for i_local in 0..<64 {
-            local_pos := base_pos + rv.Vec3{
+            local_pos := base_pos + [3]f32{
                 f32(i_local % 8) * 4 - 14,
                 f32(i_local / 8) * 4 - 14,
                 -0.05,
@@ -225,7 +225,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     // LL
 
     for i in 0..<64 {
-        base_pos := rv.Vec3{
+        base_pos := [3]f32{
             64  + f32(i % 8) * (32 + 4),
             512 + f32(i / 8) * (32 + 4),
             0.1,
@@ -253,7 +253,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
         )
 
         for i_local in 0..<64 {
-            local_pos := base_pos + rv.Vec3{
+            local_pos := base_pos + [3]f32{
                 f32(i_local % 8) * 4 - 14,
                 f32(i_local / 8) * 4 - 14,
                 -0.05,
@@ -278,7 +278,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     rv.draw_text("LMB to spawn particles", {10, 10, 0}, scale = 2)
 
     rv.submit_layers()
-    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, clear_color = rv.Vec3{0, 0, 0.1}, clear_depth = true)
+    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, clear_color = [3]f32{0, 0, 0.1}, clear_depth = true)
 
     return state
 }

examples/bouncing_ball/bouncing_ball_example.odin
@@ -17,8 +17,8 @@ State :: struct {
 }
 
 Ball :: struct {
-    position: rv.Vec3,
-    speed: rv.Vec2,
+    position: [3]f32,
+    speed: [2]f32,
     radius: f32,
 }
 
@@ -95,7 +95,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     rv.draw_text("PRESS SPACE to PAUSE BALL MOVEMENT", {20, 20, 0}, scale = 4, col = {0, 0, 0, 1})
 
     rv.submit_layers()
-    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, clear_color = rv.Vec3{.98, .98, .98}, clear_depth = true)
+    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, clear_color = [3]f32{.98, .98, .98}, clear_depth = true)
 
     return state
 }

examples/collision/collision_example.odin
@@ -13,9 +13,9 @@ import "core:math"
 state: ^State
 
 State :: struct {
-    cam_pos:    rv.Vec3,
-    cam_ang:    rv.Vec3,
-    cam_vel:    rv.Vec3,
+    cam_pos:    [3]f32,
+    cam_ang:    [3]f32,
+    cam_vel:    [3]f32,
     arena:      coll.Arena_Handle,
     mesh:       coll.Mesh_Handle,
     radius:     f32,
@@ -72,9 +72,9 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     delta := rv.get_delta_time()
 
     // Flycam controls
-    mat: rv.Mat3
+    mat: rv.matrix[3, 3]f32
     {
-        move: rv.Vec3
+        move: [3]f32
         if rv.get_key_down(.D) do move.x += 1
         if rv.get_key_down(.A) do move.x -= 1
         if rv.get_key_down(.W) do move.z += 1
@@ -353,7 +353,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     rv.draw_perf_scopes({10, 60, 0.1})
 
     rv.submit_layers()
-    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, rv.Vec3{0, 0, 0.1}, true)
+    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, [3]f32{0, 0, 0.1}, true)
     rv.render_layer(1, rv.DEFAULT_RENDER_TEXTURE, nil, true)
 
     return state

examples/draw_2d/draw_2d_example.odin
@@ -56,7 +56,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     rv.draw_text_2d("Hello Screen!\n(screenspace text)", {10, 10})
 
     rv.submit_layers()
-    rv.render_layer(0, clear_color = rv.Vec3{0, 0, 0.1}, clear_depth = true)
+    rv.render_layer(0, clear_color = [3]f32{0, 0, 0.1}, clear_depth = true)
     rv.render_layer(1, clear_color = nil, clear_depth = false)
 
     return state

examples/draw_3d/draw_3d_example.odin
@@ -9,8 +9,8 @@ import "core:math"
 state: ^State
 
 State :: struct {
-    cam_pos:    rv.Vec3,
-    cam_ang:    rv.Vec3,
+    cam_pos:    [3]f32,
+    cam_ang:    [3]f32,
     shader:     rv.Pixel_Shader_Handle,
 }
 
@@ -57,7 +57,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
 
     // Flycam controls
 
-    move: rv.Vec3
+    move: [3]f32
     if rv.get_key_down(.D) do move.x += 1
     if rv.get_key_down(.A) do move.x -= 1
     if rv.get_key_down(.W) do move.z += 1
@@ -108,18 +108,18 @@ _update :: proc(hot_state: rawptr) -> rawptr {
 
         rv.draw_triangle(
             pos = {
-                rv.Vec3{-0.5, 0, 0} + {-6, 0, 0},
-                rv.Vec3{ 0, 0.7, 0} + {-6, 0, 0},
-                rv.Vec3{ 0.5, 0, 0} + {-6, 0, 0},
+                [3]f32{-0.5, 0, 0} + {-6, 0, 0},
+                [3]f32{ 0, 0.7, 0} + {-6, 0, 0},
+                [3]f32{ 0.5, 0, 0} + {-6, 0, 0},
             },
             col = {rv.RED, rv.BLUE, rv.GREEN},
         )
 
         rv.draw_triangle(
             pos = {
-                rv.Vec3{-0.5, 0, 0} + {-6, 0, 0},
-                rv.Vec3{ 0, -0.7, 0} + {-6, 0, 0},
-                rv.Vec3{ 0.5, 0, 0} + {-6, 0, 0},
+                [3]f32{-0.5, 0, 0} + {-6, 0, 0},
+                [3]f32{ 0, -0.7, 0} + {-6, 0, 0},
+                [3]f32{ 0.5, 0, 0} + {-6, 0, 0},
             },
             col = {rv.RED, rv.GREEN, rv.CYAN},
         )
@@ -143,7 +143,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     rv.draw_text("Use WASD and QE to move, mouse to look", {20, 20, 0.1}, scale = math.ceil(rv._state.dpi_scale)) // DPI HACK
 
     rv.submit_layers()
-    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, rv.Vec3{0, 0, 0.1}, true)
+    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, [3]f32{0, 0, 0.1}, true)
     rv.render_layer(1, rv.DEFAULT_RENDER_TEXTURE, nil, false)
 
     return state

examples/fps/fps_example.odin
@@ -15,11 +15,11 @@ TERRAIN_SCALE :: 6
 state: ^State
 
 State :: struct {
-    pos:            rv.Vec3,
-    vel:            rv.Vec3,
-    angle:          rv.Vec3,
-    pos_spr:        [2]rv.Vec3,
-    angle_spr:      [2]rv.Vec3,
+    pos:            [3]f32,
+    vel:            [3]f32,
+    angle:          [3]f32,
+    pos_spr:        [2][3]f32,
+    angle_spr:      [2][3]f32,
     arena:          rv.Arena_Handle,
     terrain_mesh:   rv.Mesh_Handle,
     terrain:        [TERRAIN_SIZE][TERRAIN_SIZE]f16
@@ -90,7 +90,7 @@ _init :: proc() {
                     verts[i] = rv.Vertex{
                         pos = {f32(coord.x), height, f32(coord.y)},
                         col = u8(rv.remap_clamped(height, 0, 10, 0, 200)),
-                        uv = rv.Vec2{f32(coord.x), f32(coord.y)} / 16.0,
+                        uv = [2]f32{f32(coord.x), f32(coord.y)} / 16.0,
                     }
                     verts[i].pos.xz -= TERRAIN_SIZE * 0.5
                     verts[i].pos.xz *= TERRAIN_SCALE
@@ -133,7 +133,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
 
     // TODO: abstract basic flycam controls into a simple util?
 
-    move: rv.Vec2
+    move: [2]f32
     if rv.get_key_down(.D) do move.x += 1
     if rv.get_key_down(.A) do move.x -= 1
     if rv.get_key_down(.W) do move.y += 1
@@ -178,7 +178,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
         state.vel = rv.lexp(state.vel, 0, delta * 8)
     }
 
-    gun_pos := state.pos_spr[0] + mat * rv.Vec3{0.2, -0.1, 0.1}
+    gun_pos := state.pos_spr[0] + mat * [3]f32{0.2, -0.1, 0.1}
 
     if grounded {
         gun_pos.y += 0.005 * math.sin_f32(rv.get_time() * 11) * rv.remap_clamped(linalg.length(state.vel.xz), 0, 2, 0, 1)
@@ -221,7 +221,7 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     rv.draw_perf_scopes()
 
     rv.submit_layers()
-    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, rv.Vec3{0, 0, 0.1}, true)
+    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, [3]f32{0, 0, 0.1}, true)
     rv.render_layer(1, rv.DEFAULT_RENDER_TEXTURE, nil, false)
 
     return state
@@ -231,7 +231,7 @@ read_terrain :: proc(coord: [2]i32) -> f32 {
     return f32(state.terrain[clamp(coord.x, 0, TERRAIN_SIZE - 1)][clamp(coord.y, 0, TERRAIN_SIZE - 1)])
 }
 
-sample_terrain :: proc(pos: rv.Vec2) -> f32 {
+sample_terrain :: proc(pos: [2]f32) -> f32 {
     p := pos * (1.0 / TERRAIN_SCALE)
     fcoord := rv.floor(p)
     coord := [2]i32{
@@ -255,15 +255,15 @@ sample_terrain :: proc(pos: rv.Vec2) -> f32 {
     }
     else {
         // interpolating within upper right trianglele
-        sub = rv.Vec2{1.0, 1.0} - sub
+        sub = [2]f32{1.0, 1.0} - sub
         return samples[3] + sub.x * (samples[2] - samples[3]) + sub.y * (samples[1] - samples[3])
     }
 }
 
 @(require_results)
 intersect_terrain :: proc(
-    pos:        rv.Vec3,
-    dir:        rv.Vec3,
+    pos:        [3]f32,
+    dir:        [3]f32,
     tmin:       f32 = 0.15,
     tmax:       f32 = 100.0,
     step_size:  f32 = 0.025,

examples/geometry/geometry_example.odin
@@ -12,8 +12,8 @@ import "core:math"
 state: ^State
 
 State :: struct {
-    cam_pos:    rv.Vec3,
-    cam_ang:    rv.Vec3,
+    cam_pos:    [3]f32,
+    cam_ang:    [3]f32,
     anim_rot:   bool,
 }
 
@@ -67,9 +67,9 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     delta := rv.get_delta_time()
 
     // Flycam controls
-    mat: rv.Mat3
+    mat: rv.matrix[3, 3]f32
     {
-        move: rv.Vec3
+        move: [3]f32
         if rv.get_key_down(.D) do move.x += 1
         if rv.get_key_down(.A) do move.x -= 1
         if rv.get_key_down(.W) do move.z += 1
@@ -231,13 +231,13 @@ _update :: proc(hot_state: rawptr) -> rawptr {
     rv.draw_text(rv.tprintf("%f", cam_sweep.t), {20, 40, 0.1}, scale = 1)
 
     rv.submit_layers()
-    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, rv.Vec3{0, 0, 0.1}, true)
+    rv.render_layer(0, rv.DEFAULT_RENDER_TEXTURE, [3]f32{0, 0, 0.1}, true)
     rv.render_layer(1, rv.DEFAULT_RENDER_TEXTURE, nil, true)
 
     return state
 }
 
-draw_shape :: proc(shape: Shape_Kind, center: rv.Vec3) {
+draw_shape :: proc(shape: Shape_Kind, center: [3]f32) {
     tri := TRI
     for &v in tri {
         v += center
@@ -293,7 +293,7 @@ draw_shape :: proc(shape: Shape_Kind, center: rv.Vec3) {
     }
 }
 
-sweep_point_vs_shape :: proc(start: rv.Vec3, move: rv.Vec3, shape: Shape_Kind, center: rv.Vec3, range: f32 = 1) -> (t: f32, hit: [3]f32, nor: [3]f32, ok: bool) {
+sweep_point_vs_shape :: proc(start: [3]f32, move: [3]f32, shape: Shape_Kind, center: [3]f32, range: f32 = 1) -> (t: f32, hit: [3]f32, nor: [3]f32, ok: bool) {
     tri := TRI
     for &v in tri {
         v += center
@@ -360,11 +360,11 @@ TRI :: [3][3]f32{
 
 Sweep :: struct {
     t:      f32,
-    hit:    rv.Vec3,
-    nor:    rv.Vec3,
+    hit:    [3]f32,
+    nor:    [3]f32,
 }
 
-update_sweep_point_vs_shape :: proc(sweep: ^Sweep, start: rv.Vec3, move: rv.Vec3, shape: Shape_Kind, center: rv.Vec3) {
+update_sweep_point_vs_shape :: proc(sweep: ^Sweep, start: [3]f32, move: [3]f32, shape: Shape_Kind, center: [3]f32) {
     t, hit, nor, ok := sweep_point_vs_shape(start, move, shape, center, range = sweep.t)
 
     if ok && t < sweep.t {

commit 2946f18d7c05fe92e737f30f2a09537211d64701

Fix typo from rv.Quat to quaternion128
Bug was introduced in 4e84b0d5dc4fd1b81508f094ebacc5da62cdd0d4

<details open>
<summary> `bash check_all.sh` output before the fix </summary>
+ odin check build
+ odin check build -debug
+ odin check examples/hello
+ odin check examples/hello -debug
+ odin check examples/hello -define:RELEASE=true
+ odin check examples/hello -define:AUDIO_BACKEND=None
+ odin check examples/hello -define:AUDIO_BACKEND=miniaudio
+ odin check examples/hello -define:AUDIO_BACKEND=SDL3
+ odin check examples/hello -define:GPU_BACKEND=Dummy
+ odin check examples/hello -define:GPU_BACKEND=WGPU
+ odin check examples/hello -define:GPU_BACKEND=WGPU -target:js_wasm32
+ odin check examples/hello -define:PLATFORM_BACKEND=Dummy
+ odin check examples/hello -define:PLATFORM_BACKEND=JS -target:js_wasm32
+ odin check examples/hello -define:PLATFORM_BACKEND=SDL3
+ odin check examples/hello
+ odin check examples/hello_minimal
+ odin check examples/audio_viewer
+ odin check examples/geometry
+ odin check examples/collision
/home/raph/Documents/code/odin-stuff/ravn-engine/examples/collision/collision_example.odin(120:5) Warning: Declaration of 'point_t' may cause a stack overflow due to its type '[64][64]Sweep' having a size of 278528 bytes
	point_t: [N][N]coll.Sweep
	^
/home/raph/Documents/code/odin-stuff/ravn-engine/examples/collision/collision_example.odin(121:5) Warning: Declaration of 'sphere_t' may cause a stack overflow due to its type '[64][64]Sweep' having a size of 278528 bytes
	sphere_t: [N][N]coll.Sweep
	^
+ odin check examples/stress_test_3d
+ odin check examples/spatial_audio
+ odin check examples/fps
+ odin check examples/gpu_compute
+ odin check examples/draw_2d
+ odin check examples/draw_3d
+ odin check examples/snake_planet
/home/raph/Documents/code/odin-stuff/ravn-engine/examples/snake_planet/snake_planet_example.odin(18:21) Error: 'quaternion128' is not declared by 'rv'
	cam_rot:        rv.quaternion128,
	                ^~~~~~~~~~~~~~~^

examples/snake_planet/snake_planet_example.odin
@@ -15,7 +15,7 @@ state: ^State
 
 State :: struct {
     cam_pos:        [3]f32,
-    cam_rot:        rv.quaternion128,
+    cam_rot:        quaternion128,
     cam_fov:        f32,
 
     obsts:          [64]Obstacle,